a49c8f06668712ac747f99fa91c71ce586cdf7e0
sajid <sajidaliacademics@gmail.com>
2026-07-08 22:48:21 +0530

    added funtion in circulaer list

---
+++ linked-list/circular-ll/cll.c
@@ -0,0 +1,28 @@
+#include "cll.h"
+
+
+
+C_LIST createNewList(){
+
+}
+NODE createNewNode(int roll, char name[], float gpa){
+
+}
+void isEmpty(C_LIST li){
+
+}
+void printlist(C_LIST li){
+
+}
+void insert(C_LIST li ,NODE n){
+
+}
+void deleteEle(C_LIST li ,int roll){
+
+}
+NODE searchByROll(C_LIST li ,int roll){
+
+}
+NODE searchByName(C_LIST li, char name[]){
+
+}
