fd156ecec826d435858d219f9f7dae85ad966e77
sajid <sajidaliacademics@gmail.com>
2026-07-13 23:25:24 +0530

    added funtions

--- linked-list/double-ll/dll.c
+++ linked-list/double-ll/dll.c
@@ -2,22 +2,56 @@
 
 LIST createNewList()
 {
+    LIST li = malloc(sizeof(struct list));
+    li->size = 0;
+    li->head = NULL; 
+
+    return li;
 }
 
 NODE createNewNode(int value)
 {
-}
+    NODE n1 = malloc(sizeof(struct node));
+    n1->next = NULL;
+    n1->prev = NULL;
+    n1->num = value;
 
-void printList(LIST li)
-{
+    return n1;
 }
 
 void insertFisrt(NODE n1, LIST li)
 {
+    if( li->head == NULL){
+    li->head = n1;
+    n1->prev = NULL;
+    n1->next = NULL;
+    li->size++;
+    return;
+    }
+    NODE temp = li->head;
+
+    temp->prev = n1;
+    li->head = n1;
+    n1->next = temp;
+    li->size++;
+
+    return;
+
 }
 
 void insertLast(NODE n1, LIST li)
 {
+    if( li->head == NULL){
+    li->head = n1;
+    n1->prev = NULL;
+    n1->next = NULL;
+    li->size++;
+    return;
+    }
+    NODE temp = li->head;
+
+
+
 }
 
 void insertAfter(int val, int newVal, LIST li)
@@ -46,4 +80,9 @@ void isEmpty(LIST li)
 
 NODE search(LIST li, int value)
 {
+}
+
+void printList(LIST li)
+{
+    
 }
