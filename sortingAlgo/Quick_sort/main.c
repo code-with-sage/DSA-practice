6e6fa178f60052b14f9652714ed669ee983a7188
sajid <sajidaliacademics@gmail.com>
2026-07-23 23:57:21 +0530

    shored shorted list

--- sortingAlgo/Quick_sort/main.c
+++ sortingAlgo/Quick_sort/main.c
@@ -5,11 +5,11 @@
 
 int main(){
 
-    int *Arr = readfile("input/rand_1M.txt");
+    int *Arr = readfile("input/rev_sorted_1k.txt");
     printf("the file readed sucessfully\n");
     quich_sort(Arr, 0 , 999);
     printf("file sorterd sucessfully \n");
-    write_in_file(Arr , 1000000 , "output/sorted_1M_num.txt");
+    write_in_file(Arr , 1000 , "output/sorted_1kRs_num.txt");
     printf("the file written  sucessfully\n");
 
 }
